71904a3cab30ae541beae06859ab9af3c0efaeea
shahryar78f <shahryarfiroozi1378@gmail.com>
2026-02-04 14:27:27 +0330

    Add NextAuth authentication with credentials provider for user login

---
+++ src/app/api/auth/[...nextauth].ts
@@ -0,0 +1,50 @@
+import User from '@/models/User';
+import { verifyPassword } from '@/utils/auth';
+import connectDB from '@/utils/connectDB';
+import NextAuth, { NextAuthOptions } from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+
+const authOptions: NextAuthOptions = {
+  session: { strategy: 'jwt' },
+  providers: [
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        email: { label: 'Email', type: 'email' },
+        password: { label: 'Password', type: 'password' },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error('Invalid Data!');
+        }
+
+        try {
+          await connectDB();
+        } catch (error) {
+          throw new Error('Error in connecting to DB');
+        }
+
+        const user = await User.findOne({ email: credentials.email });
+        if (!user) {
+          throw new Error('User does not exist!');
+        }
+
+        const isValid = await verifyPassword(credentials.password, user.password);
+        if (!isValid) {
+          throw new Error('Username or password is incorrect!');
+        }
+
+        return { email: user.email, id: user._id.toString() };
+      },
+    }),
+  ],
+  pages: {
+    signIn: '/login',
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
+
