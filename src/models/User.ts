69dcf77f5a7ef6d2305ec6ca12e1931a18e4b73d
shahryar78f <shahryarfiroozi1378@gmail.com>
2026-02-04 14:30:04 +0330

    Add User model schema for MongoDB with fields for email, password, name, lastName, and todos

---
+++ src/models/User.ts
@@ -0,0 +1,21 @@
+import { model, models, Schema } from 'mongoose';
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  name: String,
+  lastName: String,
+  todos: [{ title: String, status: String }],
+  createdAt: { type: Date, default: () => Date.now(), immutable: true },
+});
+
+const User = models.User || model('User', userSchema);
+
+export default User;
