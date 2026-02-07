404d08c827746416fa50aeb69a33c535d2132afc
shahryar78f <shahryarfiroozi1378@gmail.com>
2026-02-07 22:46:11 +0330

    Add Tasks component to manage todo items with status updates, enhancing user interaction and organization of tasks.

---
+++ src/components/modules/Tasks.tsx
@@ -0,0 +1,51 @@
+import { Todo } from '@/types/todo';
+import { BiLeftArrow, BiRightArrow } from 'react-icons/bi';
+import { RiMastodonLine } from 'react-icons/ri';
+
+interface TasksProps {
+  data: Todo[];
+  accentColor?: string;
+  fetchTodos: () => Promise<void>;
+  back?: string;
+  next?: string;
+}
+
+function Tasks({ data, accentColor, fetchTodos, back, next }: TasksProps) {
+  const chengStatus = async (id: string, status: string) => {
+    const res = await fetch('/api/todos', {
+      method: 'PATCH',
+      body: JSON.stringify({ id, status }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+  };
+  return (
+    <div className="flex flex-col gap-6 justify-between p-2 pt-4">
+      {data.map(item => (
+        <div
+          key={item._id}
+          className="flex flex-col gap-6  p-3 rounded-md bg-white shadow-[0_0_20px_rgba(0,0,0,0.15)]"
+        >
+          <span className={`w-[50%] h-[2px] ${accentColor} block`} />
+          <div>
+            <RiMastodonLine />
+            <h4 className="text-base font-semibold">{item.title}</h4>
+            {back && (
+              <button className="flex " onClick={() => chengStatus(item._id, back)}>
+                <BiLeftArrow />
+                Back
+              </button>
+            )}
+            {next && (
+              <button className="" onClick={() => chengStatus(item._id, next)}>
+                Next
+                <BiRightArrow />
+              </button>
+            )}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Tasks;
