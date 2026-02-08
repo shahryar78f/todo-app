a64b6f0f86747cd456f2e1c78b2bf11ca89fb555
shahryar78f <shahryarfiroozi1378@gmail.com>
2026-02-08 16:32:07 +0330

    Implement session management in Home component, redirecting unauthenticated users to signup page and improving user flow during loading states.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,23 @@
+'use client';
+
 import HomePage from "@/components/template/HomePage";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
-  return (
-    
-      <HomePage/>
-    
-  );
+  const { status } = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (status === "unauthenticated") {
+      router.replace("/signup");
+    }
+  }, [status, router]);
+
+  if (status === "unauthenticated" || status === "loading") {
+    return null;
+  }
+
+  return <HomePage />;
 }
