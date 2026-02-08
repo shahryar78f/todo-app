7bbdf4e3b6b22c1ed380c530740542528f1808bf
shahryar78f <shahryarfiroozi1378@gmail.com>
2026-02-08 16:32:00 +0330

    Add logout functionality to Layout component, integrating NextAuth for user session management and enhancing user experience with a logout button for authenticated users.

--- src/components/layout/page.tsx
+++ src/components/layout/page.tsx
@@ -1,13 +1,29 @@
+'use client';
+import { signOut, useSession } from 'next-auth/react';
 import Link from 'next/link';
 import { BiMessageSquareAdd } from 'react-icons/bi';
+import { FiLogOut } from 'react-icons/fi';
 import { RxDashboard } from 'react-icons/rx';
 import { VscListSelection } from 'react-icons/vsc';
 
 function Layout({ children }: any) {
+  const { status } = useSession();
+  const logoutHandler = () => {
+    signOut();
+  };
   return (
     <div>
-      <header className="bg-blue-600 w-full p-2 pl-10 h-32 text-white text-3xl font-bold">
-        TODO APP
+      <header className="flex justify-between bg-blue-600 w-full p-2 pl-10 h-32">
+        <p className="text-white text-3xl font-bold">TODO APP</p>
+        {status === 'authenticated' && (
+          <button
+            onClick={logoutHandler}
+            className="flex items-center gap-2 border font-bold cursor-pointer text-white px-4 h-fit mt-10 py-2 rounded transition-colors duration-200 ease-in-out hover:text-red-400"
+          >
+            logout
+            <FiLogOut />
+          </button>
+        )}
       </header>
       <div className="flex absolute w-full">
         <aside className=" pl-10 w-[20%] bg-white p-6 h-full relative left-0 -top-20 rounded-tr-3xl min-h-[calc(100vh)] ">
