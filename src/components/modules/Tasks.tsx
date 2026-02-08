1c35bbb90e0e2232b8cf0736c43121891c57515f
shahryar78f <shahryarfiroozi1378@gmail.com>
2026-02-08 15:12:11 +0330

    Enhance Tasks component by adding success response handling for status updates and improving button styling for better user interaction.

--- src/components/modules/Tasks.tsx
+++ src/components/modules/Tasks.tsx
@@ -17,6 +17,8 @@ function Tasks({ data, accentColor, fetchTodos, back, next }: TasksProps) {
       body: JSON.stringify({ id, status }),
       headers: { 'Content-Type': 'application/json' },
     });
+    const data = await res.json();
+    if (data.status === 'success') fetchTodos();
   };
   return (
     <div className="flex flex-col gap-6 justify-between p-2 pt-4">
@@ -29,18 +31,26 @@ function Tasks({ data, accentColor, fetchTodos, back, next }: TasksProps) {
           <div>
             <RiMastodonLine />
             <h4 className="text-base font-semibold">{item.title}</h4>
-            {back && (
-              <button className="flex " onClick={() => chengStatus(item._id, back)}>
-                <BiLeftArrow />
-                Back
-              </button>
-            )}
-            {next && (
-              <button className="" onClick={() => chengStatus(item._id, next)}>
-                Next
-                <BiRightArrow />
-              </button>
-            )}
+            <div className={`flex justify-between ${!back && 'justify-end'}`}>
+              {back && (
+                <button
+                  className="flex items-center p-0.5 cursor-pointer rounded-sm text-yellow-700 font-semibold bg-amber-200"
+                  onClick={() => chengStatus(item._id, back)}
+                >
+                  <BiLeftArrow />
+                  Back
+                </button>
+              )}
+              {next && (
+                <button
+                  className="flex items-center p-0.5 cursor-pointer rounded-sm font-semibold text-green-800 bg-green-300 "
+                  onClick={() => chengStatus(item._id, next)}
+                >
+                  Next
+                  <BiRightArrow />
+                </button>
+              )}
+            </div>
           </div>
         </div>
       ))}
