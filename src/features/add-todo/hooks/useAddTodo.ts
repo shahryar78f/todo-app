7369bfb4edaaa03981c01790e67ff6b59f651058
shahryar78f <shahryarfiroozi1378@gmail.com>
2026-02-11 17:56:09 +0330

    Refactor addTodo API import and update types: change the import path for addTodo from '../api/addTodo.api' to '../api/todos.api' and adjust type imports from '../types/addTodo.types' to '../types/todos.types' for improved organization and consistency in the add-todo feature.

--- src/features/add-todo/hooks/useAddTodo.ts
+++ src/features/add-todo/hooks/useAddTodo.ts
@@ -1,7 +1,7 @@
 import { useMutation } from '@tanstack/react-query';
-import { addTodo } from '../api/addTodo.api';
-import type { AddTodoPayload, AddTodoResponse } from '../types/addTodo.types';
+import { addTodo } from '../api/todos.api';
 import { addTodoMutationKey } from '../queryKey';
+import type { AddTodoPayload, AddTodoResponse } from '../types/todos.types';
 
 export const useAddTodo = () => {
   return useMutation<AddTodoResponse, Error, AddTodoPayload>({
