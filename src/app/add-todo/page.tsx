b0c53ff6a31c3535cf424137c2cdf3ab5153994d
shahryar78f <shahryarfiroozi1378@gmail.com>
2026-02-11 17:54:16 +0330

    Add authentication check in AddTodo component: implement session management to redirect unauthenticated users to the signup page, improving security and user experience.

--- src/app/add-todo/page.tsx
+++ src/app/add-todo/page.tsx
@@ -1,6 +1,23 @@
+'use client';
 import AddTodoPage from '@/app/add-todo/_components/AddTodo';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 
 function AddTodo() {
+  const { status } = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (status === 'unauthenticated') {
+      router.replace('/signup');
+    }
+  }, [status, router]);
+
+  if (status === 'unauthenticated' || status === 'loading') {
+    return null;
+  }
+
   return <AddTodoPage />;
 }
 
