cbb0042aec6eb31931c5de701954f868c57e624a
shahryar78f <shahryarfiroozi1378@gmail.com>
2026-02-11 17:53:55 +0330

    Implement authentication check in Profile component: add session management to redirect unauthenticated users to the signup page, enhancing security and user experience.

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,6 +1,24 @@
-import ProfilePage from '@/components/template/ProfilePage';
+'use client'
+import ProfilePage from '@/app/profile/_components/ProfilePage';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 
 function Profile() {
+
+  const { status } = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (status === "unauthenticated") {
+      router.replace("/signup");
+    }
+  }, [status, router]);
+
+  if (status === "unauthenticated" || status === "loading") {
+    return null;
+  }
+
   return <ProfilePage />;
 }
 
