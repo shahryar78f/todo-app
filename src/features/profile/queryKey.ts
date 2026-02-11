73e6142cf6703b47ed8987829fa258eb946a1aff
shahryar78f <shahryarfiroozi1378@gmail.com>
2026-02-11 17:52:48 +0330

    Add profile query and mutation keys: introduce constants for profile query and mutation operations to streamline API interactions and enhance code organization.

---
+++ src/features/profile/queryKey.ts
@@ -0,0 +1,5 @@
+export const profileQueryKey = ['profile'] as const;
+
+export const profileCreateMutationKey = ['profile', 'create'] as const;
+
+export const profileUpdateMutationKey = ['profile', 'update'] as const;
