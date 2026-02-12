a9ac57623957d95fcd82ae29239d09997fc8b9c3
shahryar78f <shahryarfiroozi1378@gmail.com>
2026-02-12 11:34:58 +0330

    Refactor useAddTodo hook: update mutation key to use consistent todoMutationKey for improved clarity and maintainability.

--- src/features/todos/hooks/useAddTodo.ts
+++ src/features/todos/hooks/useAddTodo.ts
@@ -1,12 +1,12 @@
 import { useMutation } from '@tanstack/react-query';
 import { addTodo } from '../api/todos.api';
-import { addTodoMutationKey } from '../queryKey';
+import { todoMutationKey } from '../queryKey';
 import type { AddTodoPayload, AddTodoResponse } from '../types/todos.types';
 
 export const useAddTodo = () => {
   return useMutation<AddTodoResponse, Error, AddTodoPayload>({
     mutationFn: addTodo,
-    mutationKey: addTodoMutationKey,
+    mutationKey: todoMutationKey,
   });
 };
 
