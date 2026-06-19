82fcd40fd089d05fad0b65e54cc02a976ff38fca
shahryar78f <shahryarfiroozi1378@gmail.com>
2026-06-19 22:46:12 +0330

    style: Update `Input` component styles for improved readability and consistency

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -24,7 +24,7 @@ const Input = forwardRef<HTMLInputElement, InputProps>(
     const labelAlign = isPersian ? 'text-right' : 'text-left';
 
     const inputClasses = twMerge(
-      'w-full px-4 py-2 bg-[#F4F5FA]  shadow-[inset_4px_4px_4px_0px_#ECEEF6] text-[#495CC5] placeholder-[#96ACD5] placeholder:font-normal placeholder:text-[10px]  rounded-[10px] focus:outline-none  focus:border-transparent transition-all',
+      'w-full px-4 py-2 bg-[#F4F5FA]  shadow-[inset_4px_4px_4px_0px_#ECEEF6] text-[#495CC5] placeholder-[#96ACD5] placeholder:font-normal placeholder:text-[16px]  rounded-[10px] focus:outline-none  focus:border-transparent transition-all',
       inputTextAlign,
       placeholderAlign,
       isPersian && 'dir-rtl',
@@ -32,7 +32,7 @@ const Input = forwardRef<HTMLInputElement, InputProps>(
     );
 
     const labelClasses = twMerge(
-      'block mb-2 text-[10px] pr-2 pb-1 font-semibold text-[#2F3973]',
+      'block mb-2 text-[14px] pr-2 pb-1 font-semibold text-[#2F3973]',
       labelAlign,
       labelClassName,
     );
