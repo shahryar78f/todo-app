003622ec5cabcd1ee2103a64446441be734b3c5d
shahryar78f <shahryarfiroozi1378@gmail.com>
2026-06-28 17:00:48 +0330

    feat: Add `TodoColumn` component to display todos with customizable title and accent color

---
+++ src/components/modules/TodoColumn.tsx
@@ -0,0 +1,42 @@
+import { Todo } from '@/types/todo';
+import Tasks from './Tasks';
+
+interface TodoColumnProps {
+  title: string;
+  accentColor: string;
+  data: Todo[];
+  deleteTodo: (id: string) => void;
+  fetchTodos: () => Promise<void>;
+  next?: string;
+  back?: string;
+  className?: string;
+}
+
+function TodoColumn({
+  title,
+  accentColor,
+  data,
+  deleteTodo,
+  fetchTodos,
+  next,
+  back,
+  className = '',
+}: TodoColumnProps) {
+  return (
+    <div className={`self-start bg-white shadow-2xl rounded-[7px] xl:w-[22%] ${className}`}>
+      <div className={`${accentColor} p-2 rounded-t-[7px] text-center`}>
+        <p className="text-white font-semibold">{title}</p>
+      </div>
+      <Tasks
+        deleteTodo={deleteTodo}
+        data={data}
+        accentColor={accentColor}
+        fetchTodos={fetchTodos}
+        next={next}
+        back={back}
+      />
+    </div>
+  );
+}
+
+export default TodoColumn;
