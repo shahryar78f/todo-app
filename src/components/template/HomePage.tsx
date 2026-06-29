4ec6493fa2f176c3c5b762257c089ac8eaf22070
shahryar78f <shahryarfiroozi1378@gmail.com>
2026-06-29 18:15:24 +0330

    style: Add padding to `HomePage` layout for improved spacing and visual consistency

--- src/components/template/HomePage.tsx
+++ src/components/template/HomePage.tsx
@@ -43,7 +43,7 @@ function HomePage() {
   }
 
   return (
-    <div className="flex flex-col gap-6 xl:gap-0 xl:flex-row justify-around w-full">
+    <div className="flex flex-col gap-6 xl:gap-0 xl:flex-row justify-around w-full pt-4">
       <TodoColumn
         title="todo"
         accentColor="bg-amber-500"
